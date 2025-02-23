05444e61ef564ad825602e424e875a269c5d8f66
Yusuf <yefe1538@gmail.com>
2025-02-23 22:53:52 +0300

    Custom Component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 
-import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet, TouchableOpacity} from 'react-native';
 
 function App() {
   return (
       <SafeAreaView style={styles.container}>
-          <View style={styles.box_1} />
-          <View style={styles.box_2} />
-          <View style={styles.box_3} />
-          <View style={styles.box_4} />
+          <View style={styles.card_container}>
+              <View style={styles.card_body}>
+                  <Text style={styles.card_title}>John Snow</Text>
+                  <Text style={styles.card_text}>Winter is coming..</Text>
+              </View>
+              <TouchableOpacity style={styles.card_button_container}>
+                  <Text style={styles.card_button_title}>I LIKED</Text>
+              </TouchableOpacity>
+          </View>
       </SafeAreaView>
   );
 }
@@ -16,30 +21,40 @@ function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'column',
-        backgroundColor: 'yellow',
-        justifyContent: 'center',
+        backgroundColor: '#e0e0e0',
+    },
+    card_container: {
+        backgroundColor: 'white',
+        margin: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 10,
+    },
+    card_body: {
+        padding: 10,
+    },
+    card_title: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        margin: 10,
+        marginBottom: 3,
+    },
+    card_text: {
+        fontSize: 18,
+        margin: 10,
+        marginTop: 3,
+    },
+    card_button_container: {
+        backgroundColor: '#00c2ff',
+        padding: 10,
         alignItems: 'center',
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
     },
-    box_1: {
-        width: 75,
-        height: 75,
-        backgroundColor: 'red',
-    },
-    box_2: {
-        width: 75,
-        height: 75,
-        backgroundColor: 'blue',
-    },
-    box_3: {
-        width: 75,
-        height: 75,
-        backgroundColor: 'aqua',
-    },
-    box_4: {
-        width: 75,
-        height: 75,
-        backgroundColor: 'orange',
+    card_button_title: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 20,
     },
 });
 
